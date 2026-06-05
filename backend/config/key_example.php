72226fb524af73739851f12d23a589aafb235703
hsmorenom <sebas03272009@hotmail.com>
2026-06-05 15:25:43 -0500

    fix: Add suggestions for reCAPTCHA integration in login in key_example.php and loginControlador.php

--- backend/config/key_example.php
+++ backend/config/key_example.php
@@ -1,5 +1,6 @@
 <?php
 // Copia este archivo como keys.php y rellena los valores
 // Pídeme las claves a mí, Sebastian via wa para proteger la seguridad de tu aplicación
+// Luego, asegúrate de que keys.php esté en tu .gitignore para evitar subirlo a repositorios públicos
 define('RECAPTCHA_SECRET', '');
 define('RECAPTCHA_SITE', '');
